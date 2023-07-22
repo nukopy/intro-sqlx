741393c0343e880c54152b7090891ec56b7c0ce1
nukopy <nukopy@gmail.com>
2023-07-22 20:27:13 +0900

    feat: add Docker Compose settings for PostgreSQL

---
+++ db/sql/init/01_create_user.sql
@@ -0,0 +1,12 @@
+-- create role
+create role "myuser" with password 'mypassword' login;
+
+-- grant privileges on database
+grant all privileges on database "intro_sqlx_db" to "myuser";
+
+-- grant privileges on all tables in schema public
+grant all privileges on all tables in schema public to "myuser";
+
+-- 新しく作成されるテーブルに対する権限を設定
+alter default privileges in schema public
+grant all on tables to myuser;
